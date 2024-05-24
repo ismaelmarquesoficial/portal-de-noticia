9112fd56bb4f0021d9680b20903ee959cf21536f
ismaelmarques <ismael.net.marques@gmail.com>
2024-05-24 16:20:26 -0300

    Atualização SocialMedia

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,11 +1,12 @@
 import "@/assets/styles/header.css"
+import { SocialMedia } from "../others/redes-sociais"
 
 export const Header = function header() {
     return(
         <div className="header">
             <div className="header-top">
                 <div className="header-top--horario">...</div>
-                <div className="header-top--redeSociais"><RedesSociais/>></></div>
+                <div className="header-top--redeSociais"><SocialMedia/></div>
             </div>
             <div className="header-center">..</div>
             <div className="header-menu">..</div>
