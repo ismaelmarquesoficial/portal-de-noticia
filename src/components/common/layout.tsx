399b392fa5122ed2a938007198e1971df9838e58
ismaelmarques <ismael.net.marques@gmail.com>
2024-05-23 21:57:17 -0300

    setup

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -3,7 +3,9 @@ import "@/assets/styles/layout.css"
 export const Layout = function layout(){
   return(
     <div className="layout">
-      <h1>Layout</h1>
+      <div className="layout-left">...</div>
+      <div className="layout-center">...</div>
+      <div className="layout-right">...</div>
     </div>
   )
 }
