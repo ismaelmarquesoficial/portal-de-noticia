7a8ce00d3adfb76f6164a631d54263323ca92568
ismaelmarques <ismael.net.marques@gmail.com>
2024-05-24 15:51:10 -0300

    Div hora

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -3,7 +3,9 @@ import "@/assets/styles/header.css"
 export const Header = function header() {
     return(
         <div className="header">
-            <div className="header-top">..</div>
+            <div className="header-top">
+                <div className="header-top--horario">...</div>
+            </div>
             <div className="header-center">..</div>
             <div className="header-menu">..</div>
             <div className="header-botton">..</div>
