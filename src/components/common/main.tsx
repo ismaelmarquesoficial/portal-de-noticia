673f7d8bd51f464675bd3f75f0827b68ed87dc64
Ismael Marques <ismael.net.marques@gmail.com>
2024-05-28 21:30:01 -0300

    Inico do News

--- src/components/common/main.tsx
+++ src/components/common/main.tsx
@@ -1,10 +1,12 @@
 import "@/assets/styles/main.css"
+import { NewLayout } from "./new-list"
 
 export const Main = function main() {
     return(
         <div className="main">
-            <div className="main-left">...</div>
-            <div className="main-center">...</div>
+            <div className="main-left">
+                <NewLayout/>
+            </div>
             <div className="main-right">...</div>
         </div>
     )
--- src/components/common/new-list.tsx
+++ src/components/common/new-list.tsx
@@ -1,7 +1,17 @@
-export const NewLayout = function () {
+import PostList from '@/data/post-list.json';
+import "@/assets/styles/new-list.css"
+export const NewLayout: React.FC = function () {
     return(
         <div className="news">
-            ...
+                {PostList.map(PostList => (
+                    <div className='new'>
+                        <h1 key={PostList.id} className="new-title">
+                            {PostList.title}
+                        </h1>
+                        <img src={PostList.img} alt="" className="new-img"/>
+                        <p className="new-post">{PostList.post}</p>
+                    </div>
+                ))}
         </div>
     )
 }
