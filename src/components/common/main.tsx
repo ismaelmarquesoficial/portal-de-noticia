ef980ebe277f86a49e43474747434c20ed478f13
Ismael Marques <ismael.net.marques@gmail.com>
2024-05-30 21:06:45 -0300

    02;

--- src/components/common/main.tsx
+++ src/components/common/main.tsx
@@ -1,7 +1,12 @@
 import "@/assets/styles/main.css"
 import { NewLayout } from "./new-list"
+import { useState } from "react"
 
 export const Main = function main() {
+
+    const [openNew, setOpenNew] = useState(false);
+
+    
     return(
         <div className="main">
             <div className="main-left">
---
+++ src/components/common/openNew.tsx
@@ -0,0 +1,7 @@
+export const openNew = function () {
+    return(
+        <div>
+            
+        </div>
+    )
+}
