64845dd157d2de7507e9412f251fefc9ee07c2a4
Ismael Marques <ismael.net.marques@gmail.com>
2024-05-31 22:12:12 -0300

    07

--- src/components/common/new-list.tsx
+++ src/components/common/new-list.tsx
@@ -1,14 +1,16 @@
+"use client"
 import PostList from '@/data/post-list.json';
 import "@/assets/styles/new-list.css";
 
 export const NewLayout: React.FC = function () {
+    const handleOpenNew = (id:number) => {
+            alert(id)
+    }
     return(
         <div className="news">
                 {PostList.map(PostList => (
-                    <div className='new'>
-                        <h1 key={PostList.id} className="new-title">
-                            {PostList.title}
-                        </h1>
+                    <div className='new' >
+                        <h1 key={PostList.id} className="new-title" onClick={() =>handleOpenNew(PostList.id)}>{PostList.title}</h1>
                         <img src={PostList.img} alt="" className="new-img"/>
                         <h6>{PostList.data}</h6>
                         <p className="new-post">{PostList.post}</p>
