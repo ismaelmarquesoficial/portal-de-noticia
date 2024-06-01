2b524ce44f5443789db95aa31902831dffdb8ad8
Ismael Marques <ismael.net.marques@gmail.com>
2024-05-31 22:46:37 -0300

    08

--- src/components/common/new-list.tsx
+++ src/components/common/new-list.tsx
@@ -1,16 +1,25 @@
 "use client"
 import PostList from '@/data/post-list.json';
 import "@/assets/styles/new-list.css";
+import { useState } from 'react';
+import { OpenNew } from '@/types/type_openNew';
 
 export const NewLayout: React.FC = function () {
+
+    let [openNewTru, setOpenNew] = useState(false);
+
+
     const handleOpenNew = (id:number) => {
-            alert(id)
+        alert(id)
     }
     return(
         <div className="news">
                 {PostList.map(PostList => (
                     <div className='new' >
-                        <h1 key={PostList.id} className="new-title" onClick={() =>handleOpenNew(PostList.id)}>{PostList.title}</h1>
+                        <h1 key={PostList.id} className="new-title" 
+                            onClick={() =>handleOpenNew(PostList.id)}>
+                            {PostList.title}
+                        </h1>
                         <img src={PostList.img} alt="" className="new-img"/>
                         <h6>{PostList.data}</h6>
                         <p className="new-post">{PostList.post}</p>
--- src/components/common/openNew.tsx
+++ src/components/common/openNew.tsx
@@ -1,8 +1,10 @@
 import { OpenNew } from "@/types/type_openNew"
+import PostList from '@/data/post-list.json';
+import "@/assets/styles/OpenNew.css"
 
 export const openNew = function ({id,title,img,post,link,ads}: OpenNew) {
     return(
-        <div key={id}>
+        <div className="openNew-body" key={id}>
             <img src={img} alt="" />
             <h1>{title}</h1>
             <p>{post}</p>
